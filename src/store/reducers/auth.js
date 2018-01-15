8330242cc70d3d6cbe956a7b78913d8d0b506a9f
Zejian Shen <zesansan@gmail.com>
2018-01-14 17:04:00 -0800

    adding protected routes but hitting bug

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,9 +1,16 @@
 const DEFAULT_STATE = {
-	isAuthenticated: false
+	isAuthenticated: false,
+	user: {}
 };
 
 const authReducer = (state = DEFAULT_STATE, action) => {
 	switch (action.type) {
 		case SET_CURRENT_USER:
+			return {
+				isAuthenticated: !!Object.keys(action.user).length,
+				user: action.user
+			};
+		default:
+			return DEFAULT_STATE;
 	}
 };
