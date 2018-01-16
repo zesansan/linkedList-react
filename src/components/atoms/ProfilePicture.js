d5f01b7edf3b096fa88d87b6103e74b030c1dc7c
Zejian Shen <zesansan@gmail.com>
2018-01-16 09:50:13 -0800

    adding things back in

--- src/components/atoms/ProfilePicture.js
+++ src/components/atoms/ProfilePicture.js
@@ -2,6 +2,8 @@ import React from "react";
 import Avatar from "material-ui/Avatar";
 
 const ProfilePicture = props => (
-	<Avatar src={props.profilePicture} alt="profile-picture" size={150} />
+	<div>
+		<Avatar src={props.profilePicture} alt="profile-picture" size={150} />
+	</div>
 );
 export default ProfilePicture;
--- src/components/molecules/UserExperienceCard.js
+++ src/components/molecules/UserExperienceCard.js
@@ -9,3 +9,4 @@ const UserExperienceCard = props => (
 );
 
 export default UserExperienceCard;
+
